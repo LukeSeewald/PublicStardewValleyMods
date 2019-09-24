47eb4b29eccb2f027f0922bfb077e12c57f0fbdf
Luke Seewald <lseewald@uwaterloo.ca>
2019-09-24 00:43:22 -0400

    Made it detect buffs that were +2 or more and fixed the duration

--- PublicStardewMods/WhatAreYouMissing/ItemDisplayInfo.cs
+++ PublicStardewMods/WhatAreYouMissing/ItemDisplayInfo.cs
@@ -46,91 +46,6 @@ namespace WhatAreYouMissing
             }
         }
 
-        private string GetCookedItemStatsInfo()
-        {
-            string stats = "";
-
-            stats += Utilities.GetTranslation("ENERGY") + ": " + GetEnergyRestored() + "\n";
-            stats += Utilities.GetTranslation("HEALTH") + ": " + GetHealthRestored();
-
-            List<string> buffs = GetBuffs();
-
-            if(buffs.Count > 0)
-            {
-                stats += "\n" + Utilities.GetTranslation("BUFFS") + ": " + string.Join(", ", buffs) + "\n";
-                stats += Utilities.GetTranslation("DURATION") + ": " + GetBuffDuration() + " " + Utilities.GetTranslation("MINUTE");
-            }
-
-            return stats;
-        }
-
-        private double GetHealthRestored()
-        {
-            int edibility = Convert.ToInt32(Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoEdibilityIndex]);
-
-            return Math.Round(edibility * 2.5 * 0.4); //this is what the tooltip would show, the actual amount is edibility * 2.5 * 0.45
-        }
-
-        private double GetEnergyRestored()
-        {
-            int edibility = Convert.ToInt32(Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoEdibilityIndex]);
-
-            return Math.Round(edibility * 2.5);
-        }
-
-        private List<string> GetBuffs()
-        {
-            List<string> nameOfBuffs = new List<string>();
-            string[] buffs = Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoBuffTypesIndex].Split(' ');
-
-            for(int i = 0; i < buffs.Length; ++i)
-            {
-                if(buffs[i] == "1")
-                {
-                    nameOfBuffs.Add(GetBuffNameFromIndex(i));
-                }
-            }
-            return nameOfBuffs;
-        }
-
-        private string GetBuffNameFromIndex(int index)
-        {
-            switch (index)
-            {
-                case 0:
-                    return Utilities.GetTranslation("FARMING");
-                case 1:
-                    return Utilities.GetTranslation("FISHING");
-                case 2:
-                    return Utilities.GetTranslation("MINING");
-                case 4:
-                    return Utilities.GetTranslation("LUCK");
-                case 5:
-                    return Utilities.GetTranslation("FORAGING");
-                case 7:
-                    return Utilities.GetTranslation("MAX_ENERGY");
-                case 8:
-                    return Utilities.GetTranslation("MAGNETISM");
-                case 9:
-                    return Utilities.GetTranslation("SPEED");
-                case 10:
-                    return Utilities.GetTranslation("DEFENSE");
-                case 11:
-                    return Utilities.GetTranslation("ATTACK");
-                default:
-                    return "Oopsies";
-            }
-        }
-
-        /// <summary>
-        /// Gets it in minutes
-        /// </summary>
-        /// <returns></returns>
-        private int GetBuffDuration()
-        {
-            return Convert.ToInt32(Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoBuffDurationIndex]) / 100 + 1;
-        }
-
         private string GetFishItemDisplayInfo()
         {
             Constants constants = new Constants();
@@ -247,6 +162,103 @@ namespace WhatAreYouMissing
         {
             Constants constants = new Constants();
             return constants.SPECIAL_COOKING_IDS.Contains(itemCategory);
+        }
+
+        private string GetCookedItemStatsInfo()
+        {
+            string stats = "";
+
+            stats += Utilities.GetTranslation("ENERGY") + ": " + GetEnergyRestored() + "\n";
+            stats += Utilities.GetTranslation("HEALTH") + ": " + GetHealthRestored();
+
+            Dictionary<string, string> buffs = GetBuffs();
+
+            if (buffs.Count > 0)
+            {
+                stats += "\n" + Utilities.GetTranslation("BUFFS") + ": ";
+                foreach(KeyValuePair<string, string> buffIncreasePair in buffs)
+                {
+                    stats += "+" + buffIncreasePair.Value + " " + buffIncreasePair.Key + ", ";
+                }
+                stats = stats.Substring(0, stats.Length - 2) + "\n";
+                int durationMin = GetBuffDuration()[0];
+                int durationSec = GetBuffDuration()[1];
+                stats += Utilities.GetTranslation("DURATION") + ": " + durationMin + " " + Utilities.GetTranslation("MINUTE") + " " + durationSec + " " + Utilities.GetTranslation("SECONDS");
+            }
+
+            return stats;
+        }
+
+        private double GetHealthRestored()
+        {
+            int edibility = Convert.ToInt32(Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoEdibilityIndex]);
+
+            return Math.Round(edibility * 2.5 * 0.45); //this is what it actually is, not necessarily what the tooltip shows.
+        }
+
+        private double GetEnergyRestored()
+        {
+            int edibility = Convert.ToInt32(Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoEdibilityIndex]);
+
+            return Math.Round(edibility * 2.5);
+        }
+
+        private Dictionary<string, string> GetBuffs()
+        {
+            Dictionary<string, string> nameOfBuffs = new Dictionary<string, string>();
+            string[] buffs = Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoBuffTypesIndex].Split(' ');
+
+            for (int i = 0; i < buffs.Length; ++i)
+            {
+                if (buffs[i] != "0")
+                {
+                    nameOfBuffs.Add(GetBuffNameFromIndex(i), buffs[i]);
+                }
+            }
+            return nameOfBuffs;
+        }
+
+        private string GetBuffNameFromIndex(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return Utilities.GetTranslation("FARMING");
+                case 1:
+                    return Utilities.GetTranslation("FISHING");
+                case 2:
+                    return Utilities.GetTranslation("MINING");
+                case 4:
+                    return Utilities.GetTranslation("LUCK");
+                case 5:
+                    return Utilities.GetTranslation("FORAGING");
+                case 7:
+                    return Utilities.GetTranslation("MAX_ENERGY");
+                case 8:
+                    return Utilities.GetTranslation("MAGNETISM");
+                case 9:
+                    return Utilities.GetTranslation("SPEED");
+                case 10:
+                    return Utilities.GetTranslation("DEFENSE");
+                case 11:
+                    return Utilities.GetTranslation("ATTACK");
+                default:
+                    return "Oopsies";
+            }
+        }
+
+        /// <summary>
+        /// Gets it in minutes seconds
+        /// </summary>
+        /// <returns></returns>
+        private int[] GetBuffDuration()
+        {
+            int[] result = new int[2];
+            double value = Convert.ToDouble(Game1.objectInformation[ParentSheetIndex].Split('/')[SObject.objectInfoBuffDurationIndex]) * 0.7;
+
+            result[0] = (int)value / 60;
+            result[1] = (int)value % 60;
+            return  result;
         }
 
         private List<string> GetSeasonsToCatch()
